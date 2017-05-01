f6a7fe16e6c36d74ea9048656c5f28402e7ab743
Jarad Touchberry <you@example.com>
2017-05-01 08:19:57 -0400

    need to fix delete button

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,45 +11,47 @@ public class Main {
 
     public static void createTables(Connection conn) throws SQLException {
         Statement stmt = conn.createStatement();
-        stmt.execute("CREATE TABLE IF NOT EXISTS User (id INTEGER, userName VARCHAR, address VARCHAR, email VARCHAR)");
+        stmt.execute("CREATE TABLE IF NOT EXISTS Users (id INTEGER, name VARCHAR, address VARCHAR, email VARCHAR)");
     }
 
-    public static void insertUser(Connection conn, String userName, String address, String email) throws SQLException {
-        PreparedStatement stmt = conn.prepareStatement("INSERT INTO User VALUES (NULL, ?, ?, ?)");
-        stmt.setString(1, userName);
-        stmt.setString(2, address);
-        stmt.setString(3, email);
+    public static void insertUser(Connection conn, String name, String email, String address) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO users VALUES (NULL, ?, ?, ?)");
+        stmt.setString(1, name);
+        stmt.setString(2, email);
+        stmt.setString(3, address);
         stmt.execute();
     }
 
-    public static ArrayList<User> selectUsers(Connection conn) throws SQLException {
+    public static ArrayList<User> getUsers(Connection conn) throws SQLException {
         ArrayList<User> users = new ArrayList<>();
-        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users");
+        PreparedStatement stmt = conn.prepareStatement("Select * FROM users");
         ResultSet results = stmt.executeQuery();
+
         while (results.next()) {
-            Integer id = results.getInt("id");
-            String userName = results.getString("userName");
+            String name = results.getString("name");
             String address = results.getString("address");
             String email = results.getString("email");
-            users.add(new User(id, userName, address, email));
+
+            User u = new User(address, email, name);
+            users.add(u);
         }
         return users;
     }
 
     public static void updateUser(Connection conn, User user) throws SQLException {
-        PreparedStatement stmt = conn.prepareStatement("UPDATE users SET username=?, address=?, email=?, WHERE id=?");
-        stmt.setString(1, user.getUserName());
+        PreparedStatement stmt = conn.prepareStatement("UPDATE users WHERE username=?, address=?, email=?, WHERE id=?");
+        stmt.setString(1, user.getName());
         stmt.setString(2, user.getAddress());
         stmt.setString(3, user.getEmail());
         stmt.setInt(4, user.getId());
     }
 
     public static void deleteUser(Connection conn, Integer id) throws SQLException {
-        PreparedStatement stmt = conn.prepareStatement("DELETE * FROM user Where id = ?");
+        PreparedStatement stmt = conn.prepareStatement("DELETE * FROM users Where id = ?");
         stmt.setInt(1, id);
         stmt.execute();
     }
-    
+
     public static void main(String[] args) throws SQLException {
         Server.createWebServer().start();
         Spark.staticFileLocation("/public");
@@ -58,29 +60,29 @@ public class Main {
         Spark.init();
 
         Spark.get(
-                "/get-users",
+                "/user",
                 ((request, response) -> {
-                    ArrayList<User> users = selectUsers(conn);
+                    ArrayList<User> users = getUsers(conn);
                     JsonSerializer s = new JsonSerializer();
                     return s.serialize(users);
                 })
         );
 
         Spark.post(
-                "add-user",
+                "/user",
                 ((request, response) -> {
                     String body = request.body();
                     JsonParser p = new JsonParser();
                     User user = p.parse(body, User.class);
-                    insertUser(conn, user.getUserName(), user.getAddress(), user.getEmail());
+                    insertUser(conn, user.getName(), user.getEmail(), user.getAddress());
                     return "";
                 })
         );
 
         Spark.delete(
                 "/user/:id",
-                (req, rep) -> {
-                    Integer id = Integer.valueOf(req.params(":id"));
+                (req, res) -> {
+                    Integer id = Integer.valueOf(req.params("id"));
                     deleteUser(conn, id);
                     return "";
                 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -5,21 +5,22 @@ import java.util.ArrayList;
  * Created by jaradtouchberry on 4/28/17.
  */
 public class User {
-    private Integer Id;
-    private String userName;
-    private String address;
-    private String email;
+    public Integer Id;
+    public String address;
+    public String email;
+    public String name;
     public static ArrayList<User> users = new ArrayList<>();
 
     public User() {
 
     }
 
-    public User(Integer id, String userName, String address, String email) {
-        Id = id;
-        this.userName = userName;
+    public User(String address, String email, String name) {
         this.address = address;
         this.email = email;
+        this.name = name;
+
+
     }
 
     public Integer getId() {
@@ -28,14 +29,6 @@ public class User {
 
     public void setId(Integer id) {
         Id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
     }
 
     public String getAddress() {
@@ -53,6 +46,14 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
 
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
 
